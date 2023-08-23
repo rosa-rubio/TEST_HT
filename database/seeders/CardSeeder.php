38930177e85c81a79e0c0b150b86c8a5ac05af04
Sylvia Llorente <sylviall81@gmail.com>
2023-08-23 11:34:50 +0200

    8 nuevos registros para el seeder

--- database/seeders/CardSeeder.php
+++ database/seeders/CardSeeder.php
@@ -25,28 +25,62 @@ class CardSeeder extends Seeder
 
         $cards = [
             [
-                'image' => 'https://www.mondosonoro.com/wp-content/uploads/2020/10/Woodkid-S16-disco.jpg',
-                'title' => 'Fun Vacation',
-                'location' => 'Puerto Rico',
-                'description' => 'Playa, sol y Arena'
+                'image' => 'https://s1.abcstatics.com/media/viajar/2018/04/25/massai-mara-U10111949550xbH--510x349@abc.jpg',
+                'title' => 'Safari de aventura',
+                'location' => 'Kenia',
+                'description' => 'Sientes la llamada de la naturaleza mientras elefantes y leones vagan libremente a tu alrededor. Esta experiencia vale absolutamente la pena cada segundo y una aventura para toda la vida'
             ],
             
             [
-                'image' => 'https://www.mondosonoro.com/wp-content/uploads/2020/10/Woodkid-S16-disco.jpg',
-                'title' => 'Extreme travel',
-                'location' => 'Everest',
-                'description' => 'Snow and sport'
+                'image' => 'https://s1.abcstatics.com/media/viajar/2018/04/25/rodas-kK6B--510x349@abc.jpg',
+                'title' => 'Escapada Soñada',
+                'location' => 'Grecia',
+                'description' => 'Todo es mejor en Grecia, donde el vino es más suave, las aguas son más claras y las puestas de sol son más intensas. Los viajeros nunca quieren irse'
             ],
 
             [
-                'image' => 'https://www.mondosonoro.com/wp-content/uploads/2020/10/Woodkid-S16-disco.jpg',
-                'title' => 'Romantic Vacation',
-                'location' => 'Italia',
-                'description' => 'Pizza, ice cream'
+                'image' => 'https://s2.abcstatics.com/media/viajar/2018/04/25/antartida-kK6B--510x349@abc.jpg',
+                'title' => 'Vacaciones en el Hielo',
+                'location' => 'Antartida',
+                'description' => 'Imagina una ballena emergiendo a tu lado para saludarte mientras los pingüinos brincan en la proa de tu bote cuando navegas junto a un iceberg en kayak. Esto es la Antártida'
+            ],
+
+            [
+                'image' => 'https://s1.abcstatics.com/media/viajar/2018/04/25/islandia-kK6B--510x349@abc.jpg',
+                'title' => 'Auroras Boreales y naturaleza',
+                'location' => 'Islandia',
+                'description' => 'Difícilmente hay un país en el mundo que luzca tantos paisajes trascendentales en tan poco tiempo. La belleza te atrae. La diversidad hace que nunca quieras irte'
+            
+            ],
+            [
+                'image' => 'https://s3.abcstatics.com/media/viajar/2018/04/25/maldivas-kK6B--510x349@abc.jpg',
+                'title' => 'Playas paradisiacas y paz',
+                'location' => 'Maldivas',
+                'description' => 'Despertarse con el runrún del mar en su idílico bungaló en las Maldivas dará vida a la realidad tropical'
+            ],
+            [
+                'image' => 'https://es.wikipedia.org/wiki/Salto_%C3%81ngel#/media/Archivo:SaltoAngel1.jpg',
+                'title' => 'Salto Angel, una maravilla natural',
+                'location' => 'Venezuela',
+                'description' => 'El Salto Ángel es una de las mayores atracciones turísticas de Venezuela. Su sola visión te dejará sin aliento. Una experiencia indescriptible que debes vivir'
+            ],
+            [
+                'image' => 'https://s1.abcstatics.com/media/viajar/2018/04/25/galapagos-kK6B--510x349@abc.jpg',
+                'title' => 'Islas Galapagos, el origen de las especies',
+                'location' => 'Ecuador',
+                'description' => 'Un viaje en el tiempo para entender por qué Darwin encontró su tierra de ensueño aquí. La flora y la fauna abundantes y las especies asombrosas dan color al ambiente en medio de la emoción de la aventura'
+            ],
+
+            [
+                'image' => 'https://s1.abcstatics.com/media/viajar/2018/04/25/galapagos-kK6B--510x349@abc.jpg',
+                'title' => 'Machu Pichu, tribus e historia',
+                'location' => 'Perú',
+                'description' => 'Pasear por el Camino Inca es un intenso viaje a siglos pasados y a las raíces de nuestra historia que permanecen vivas hasta el día de hoy'
             ]
-            
-        ];
-            
+
+            ];
+          
         DB::table('cards')->insert($cards);
+        
     }
 }
