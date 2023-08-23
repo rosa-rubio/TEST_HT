7665c7ad446f9a7481c3c5c8384caa5c8df226a0
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-08-23 18:31:29 +0200

    modal delete styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,7 +16,7 @@
                     </div>
 
                     @if ($form_add_new_card->user->is(auth()->user()))
-                    <div class="flex space-x-2">
+                    <div class="flex flex-icons">
                         <a href="{{ route('details', $form_add_new_card) }}"
                             class="text-indigo-600 hover:text-indigo-900">
                             <img src="{{ asset('assets/Info-icon.svg') }} " alt="Mostrar mas info"></a>
@@ -29,24 +29,25 @@
 
                         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                             aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-body">
-                                        ¿Quieres eliminar este destino?
+                            <div class="modal-dialog modal-width">
+                                <div class="modal-content rounded-4">
+                                    <div class="modal-body rounded-4">
+                                        <p>¿Quieres eliminar este destino?</p>
+                                        <div class="buttons">
+                                            <form method="POST"
+                                                action="{{ route('formAddNewCard.destroy', $form_add_new_card) }}">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="rounded-pill btn-accept button"
+                                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                                    Aceptar
+                                                </button>
+                                            </form>
+                                            <button type="button" class="btn-cancel button rounded-pill"
+                                                data-bs-dismiss="modal">Cancelar</button>
+                                        </div>
                                     </div>
-                                    <div class="">
-                                        <button type="button" class="btn btn-secondary"
-                                            data-bs-dismiss="modal">Cancelar</button>
-                                        <form method="POST"
-                                            action="{{ route('formAddNewCard.destroy', $form_add_new_card) }}">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="text-red-600 hover:text-red-900"
-                                                onclick="event.preventDefault(); this.closest('form').submit();">
-                                                Aceptar
-                                            </button>
-                                        </form>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -7,9 +7,9 @@
             <div class="flex head-details">
                 <div>
                     <h1 class="title-details">{{ $form_add_new_card->title }}</h1>
-                    <h2 class="location-details">{{ $form_add_new_card->location }}</h2>
+                    <h3 class="location-details">{{ $form_add_new_card->location }}</h3>
                 </div>
-                <div class="flex">
+                <div class="flex-icons flex">
                     <a href="{{ route('formAddNewCard.edit', $form_add_new_card) }}"
                         class="text-indigo-600 hover:text-indigo-900"><img src="{{ asset('assets/Edit-icon.svg') }}"
                             alt="Editar"></a>
@@ -20,24 +20,25 @@
 
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                         aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-body">
-                                    ¿Quieres eliminar este destino?
+                        <div class="modal-dialog modal-width">
+                            <div class="modal-content rounded-4">
+                                <div class="modal-body rounded-4">
+                                    <p>¿Quieres eliminar este destino?</p>
+                                    <div class="buttons">
+                                        <form method="POST"
+                                            action="{{ route('formAddNewCard.destroy', $form_add_new_card) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit" class="rounded-pill btn-accept button"
+                                                onclick="event.preventDefault(); this.closest('form').submit();">
+                                                Aceptar
+                                            </button>
+                                        </form>
+                                        <button type="button" class="btn-cancel button rounded-pill"
+                                            data-bs-dismiss="modal">Cancelar</button>
+                                    </div>
                                 </div>
-                                <div class="">
-                                    <button type="button" class="btn btn-secondary"
-                                        data-bs-dismiss="modal">Cancelar</button>
-                                    <form method="POST"
-                                        action="{{ route('formAddNewCard.destroy', $form_add_new_card) }}">
-                                        @csrf
-                                        @method('delete')
-                                        <button type="submit" class="text-red-600 hover:text-red-900"
-                                            onclick="event.preventDefault(); this.closest('form').submit();">
-                                            Aceptar
-                                        </button>
-                                    </form>
-                                </div>
+
                             </div>
                         </div>
                     </div>
