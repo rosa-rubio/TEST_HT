539605887efd49184b87633b9b61fc0ddafdec26
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-08-24 22:27:37 +0200

    search result style

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -2,7 +2,7 @@
     <div class="grid-container">
         @foreach ($cards as $card)
         <div class="card-container">
-            <div class="image-container">
+            <div class="img-container">
                 <img class="image-card" src="{{ $card->image }}" alt="{{ $card->title }}">
             </div>
             <h3 class="card-title">{{ $card->title }}</h3>
@@ -11,7 +11,7 @@
         @endforeach
         @foreach ($form_add_new_cards as $form_add_new_card)
         <div class="card-container">
-            <div class="image-container">
+            <div class="img-container">
                 <img class="image-card" src="{{ asset('storage/' . $form_add_new_card->image) }}" alt="Image">
             </div>
             <h3 class="card-title">{{ $form_add_new_card->title }}</h3>
